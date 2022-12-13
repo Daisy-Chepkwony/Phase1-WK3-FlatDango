e8f2db1ac2ab683086f2101abe714d386d551c57
+ <+>
2022-12-13 14:16:56 +0300

    movies information

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
   }
 //   // it creates list for the movie names
   function moviesdata(film) {   
-      const menu=document.querySelector("div#movies-menu");
+      const menu=document.querySelector("div#movies-tickets");
        const menuLi=document.createElement("ul");
       const menuBar=document.createElement("li");
       menuBar.innerHTML=film.title;
@@ -19,40 +19,59 @@ document.addEventListener("DOMContentLoaded", ()=> {
        menuBar.style.cursor="pointer";
        menu.appendChild(menuLi);
        menuBar.addEventListener("click",()=>{
-          movieInfo(film);
+          moviesAvailable(film);
        });
   }
 //   //getting the movies info
-  function movieInfo(film) {
-     const movie = document.querySelector("#name");
+  function moviesAvailable(film) {
+     const movie = document.querySelector("#moviesName");
      movie.innerHTML = film.title;
-     const image = document.querySelector("#posters");
+     const image = document.querySelector("#movies");
      image.src = film.poster;
-     const runtime = document.querySelector("#runtime");
-     runtime.innerHTML = `${film.runtime}MINUTES`;
-     const showtime = document.querySelector("#showtime");
-     showtime.innerHTML = film.showtime;
-    const availabletkts = document.querySelector("#available-tickets");
-     availabletkts.innerHTML = film.tickets_sold;
+     const moviesTime = document.querySelector("#moviestime");
+     moviesTime.innerHTML = `${film.moviesTime}MINUTES`;
+     const displaytime = document.querySelector("#displaytime");
+     displaytime.innerHTML = film.displaytime;
+    const queuetickets = document.querySelector("#queue-tickets");
+    queuetickets.innerHTML = film.tickets_sold;
      const capacity=document.querySelector("#capcity");
      capacity.innerHTML=film.capacity;
+     //added
+     const b=document.querySelector("#btns")
+     const btns=document.createElement("button");
+      btns.innerHTML="BookMyShow";
+       btns.id="ticket";
+       b.appendChild(btns);
+       btns.addEventListener("click",()=>{
+            if  (film.tickets_sold< film.capacity){
+             film.tickets_sold +=1;
+             queuetickets.innerHTML = ` tickets sold:${film.tickets_sold}`;
+            
+          }else {
+            queuetickets.innerHTML=`soldout`
+           }
+          })
+     
    }
+   //added
+ 
+
 //    // create a button that will work on the tickects
-   function capacitydetails(film){
-      const buttons=document.querySelector("#btn");
-      const btns=document.createElement("button");
-      buttons.appendChild(btns);
-      buttons.addEventListener("click",()=>{
-        let bts=document.getElementById("button");
-          if  (bts>0){
-            document.getElementById("bts").textcontent=bts-1
-          }else
-          if(parseInt(bts===0)){
-            document.getElementById("buyticket")="soldout"
-          }
-      })
+  //  function capacitydetails(film){
+  //     const buttons=document.querySelector("#btn");
+  //     const btns=document.createElement("button");
+  //     buttons.appendChild(btns);
+  //     buttons.addEventListener("click",()=>{
+  //       let bts=document.getElementById("button");
+  //         if  (bts>0){
+  //           document.getElementById("bts").textcontent=bts-1
+  //         }else
+  //         if(parseInt(bts===0)){
+  //           document.getElementById("buyticket")="soldout"
+  //         }
+  //     })
   
-   }
+  //  }
 // //   // function moviesmenu(film) {
 // //   //   const bar = document.querySelector("#filmes");
 // //   //   bar.innerHTML = film.title;
